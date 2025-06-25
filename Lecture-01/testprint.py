6f3a042472ff4216684545983362258a937a144c
sukanya125.git <sukanyamaphrayuen@gmail.com>
2025-06-25 11:13:43 +0700

    add testnum.py

--- Lecture-01/testprint.py
+++ Lecture-01/testprint.py
@@ -1,4 +1,10 @@
 print('Hello, woeld!')
 print('Sukanya Maphrayuen')
 
+print("I'm hungry")
+print("I'm good \"And I will do my bast\"")
+print("\ta\na\ta\ta\n\ta")
+print('''\ta
+a\ta\ta
+\ta''')
 
