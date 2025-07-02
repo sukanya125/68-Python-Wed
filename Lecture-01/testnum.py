a09ab24f8ab6c0a5d913b6909022395ebb245ac8
sukanya125.git <sukanyamaphrayuen@gmail.com>
2025-07-02 11:02:05 +0700

    02

---
+++ Lecture-01/testnum.py
@@ -0,0 +1,14 @@
+#%d จำนวนเต็ม
+#%f จำนวนทศนิยม
+#%g จำนนวนเต็มหรือทศนิยม
+#%s ข้อความ
+
+from math import pi
+
+print("%d" %100)
+print("%d" %100.56)
+print("%f"%100.56)
+print("%.2f" %100.56)
+print("%.4f" %pi)
+print("My age is " + str(20) + " i have %2.f Bth." %(3500.50))
+print(5+4)
