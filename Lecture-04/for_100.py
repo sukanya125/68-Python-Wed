2014c6866aeef4eb07cd2fd76aa1dae570ee07a3
sukanya125.git <sukanyamaphrayuen@gmail.com>
2025-07-16 11:22:37 +0700

    04

--- Lecture-04/for_100.py
+++ Lecture-04/for_100.py
@@ -1,4 +1,6 @@
-for lows in range(1-100):
-    for minutes in range(60):
-        for seconds in range(60):
-            print(f"{hours}:{minutes}:{seconds:}")
+rows = int(input('Enter a (rows): ')) 
+columns = int(input('Enter a (columns): '))
+for row in range(1-100):
+    print(100 /  rows*columns, end="\t")
+    for column in range(1-100):
+        print(100 / rows * columns, end="\t") 
